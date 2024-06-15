ee2a043c87c9139584d842de4f91c30e242d2e54
Byun SoYun <bsy1482@naver.com>
2024-06-15 12:55:24 +0900

    feat: 블로그 게시물 탐색 후 해당 게시물 조회 과정 구현

--- automation.py
+++ automation.py
@@ -2,6 +2,8 @@ import time
 import json
 
 from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 
 def make_url(query):
@@ -11,7 +13,7 @@ def make_url(query):
     :param query: 검색어 키워드
     :return: 검색 키워드가 포함된 URL
     """
-    return f"https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&q={query}"
+    return f"https://search.daum.net/search?w=tot&DA=PGD&q={query}"
 
 
 def load_data():
@@ -29,6 +31,42 @@ def load_data():
     return json_data
 
 
+def find_element(driver, website_url):
+    """
+    `website_url`에 해당하는 블로그 게시물 HTML 요소를 탐색 후 반환합니다.
+
+    :param driver: Chrome Driver
+    :param website_url: 블로그 URL
+    :return: 블로그 게시물 HTML 요소 | None
+    """
+    cards = driver.find_elements(By.TAG_NAME, "c-card")
+
+    for card in cards:
+        try:
+            eyebrow = card.find_element(By.TAG_NAME, "c-frag")
+
+            if eyebrow.text == website_url:
+                return card
+        except NoSuchElementException:
+            pass
+
+    return None
+
+
+def find_anchor(element):
+    """
+    `element`에서 `a` 태그를 탐색 후 반환합니다.
+
+    :param element: 탐색 대상
+    :return: `a` 태그 | None
+    """
+    try:
+        body = element.find_element(By.TAG_NAME, "c-doc-web")
+        return body.find_element(By.TAG_NAME, "a")
+    except NoSuchElementException:
+        return None
+
+
 if __name__ == '__main__':
     # Chrome Driver 생성
     driver = webdriver.Chrome()
@@ -39,11 +77,33 @@ if __name__ == '__main__':
     website_url = data["website_url"]
     queries = data["queries"]
 
-    # 검색어를 순환하며 자동화를 수행합니다.
+    # 검색어를 순회하며 자동화를 수행합니다.
     for query in queries:
         url = make_url(query)
 
+        # URL 접근
         driver.get(url)
+        time.sleep(1)
+
+        # 태그 분석 및 게시물 탐색
+        element = find_element(driver, website_url)
+        if element is None:
+            print(f"해당 검색어({query})의 검색 결과로는 블로그 게시물을 찾지 못했습니다.")
+            continue
+
+        anchor = find_anchor(element)
+        if anchor is None:
+            print(f"해당 검색어({query})의 블로그 게시물 요소 내에서 `a` 태그를 찾지 못하였습니다.")
+            continue
+
+        # 블로그 게시물 열기 및 활성 탭 변경 후 1초 대기
+        anchor.click()
+        driver.switch_to.window(driver.window_handles[1])
+        time.sleep(1)
+
+        # 블로그 게시물 탭 닫기 및 활성 탭 변경 후 0.25초 대기
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
         time.sleep(0.25)
 
     # Chrome Driver 종료
